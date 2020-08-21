82cc4998d9cf40090541b6187b0cd20a018dcca9
Riyas Rawther <riyasrawther.in@gmail.com>
2020-08-21 17:24:22 +0100

    Tested with Ubuntu 20.04

--- lempinstall.sh
+++ lempinstall.sh
@@ -1,53 +1,8 @@
 #!/bin/sh
 
 # Author : Riyas Rawther
-# Email: riyasrawther.in@gmail.com
-# Phone: +91-62381-22062
 
-
-#Set up MariaDB repositories
-
-apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu focal main'
-Set up NGINX Stable repositories
-echo "setting up the apt repository for stable nginx packages"
-
-echo "deb http://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx" | sudo tee -a /etc/apt/sources.list
-	
-	sudo wget https://nginx.org/keys/nginx_signing.key
-sudo apt-key add nginx_signing.key
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
-	
-
-# compile Nginx from the official repo with brotli and pagespeed compression
-
-[ ! -d ${HOME}/log ] && mkdir ${HOME}/log
-
-G_DIR="$(pwd)"
-
-# logging everything
-log_file=${HOME}/log/brotli.log
-exec > >(tee -a ${log_file} )
-exec 2> >(tee -a ${log_file} >&2)
-
-# Defining return code check function
-check_result() {
-    if [ $1 -ne 0 ]; then
-        echo "Error: $2"
-        exit $1
-    fi
-}
-
-export DEBIAN_FRONTEND=noninteractive
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
-printf '%-72s' "Installing pre-requisites..."
-sudo apt-get -qq install dpkg-dev  git  uuid-dev build-essential zlib1g-dev libpcre3 libpcre3-dev unzip fail2ban ufw curl gnupg2 ca-certificates lsb-release
-echo done.
-
+apt-get update; apt-get upgrade -y; apt-get install -y fail2ban ufw;
 # SSH, HTTP and HTTPS
 ufw allow 22
 ufw allow 80
@@ -62,103 +17,25 @@ ufw allow 443
 ufw --force enable
 
 #Add some PPAs to stay current
-apt-get install -y software-properties-common
+#apt-get install -y software-properties-common
 #apt-add-repository ppa:ondrej/apache2 -y
 #apt-add-repository ppa:ondrej/nginx-mainline -y
 #apt-add-repository ppa:ondrej/php -y
 
+#Set up MariaDB repositories
 
+apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu focal main'
 
 #Install base packages
-apt-get install -y build-essential tree curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools
+apt-get update; apt-get install -y build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools
 
 #Set the timezone to UTC
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-
-
-printf '%-72s' "Updating apt repos..."
-sudo apt-get -qq update
-echo done.
-
-sudo install -o ${UID} -g $(id -gn $USER) -d /usr/local/src/${USER}
-cd /usr/local/src/${USER}
-
-sudo apt-get build-dep nginx -y
-apt-get source nginx
-
-
-#brotli 
-
-git clone --recursive https://github.com/eustas/ngx_brotli
-sudo ln -s /usr/local/src/${USER}/ngx_brotli /usr/local/src/ngx_brotli
-sudo chown ${USER}:$USER:$(id -gn $USER) /usr/local/src/ngx_brotli
-cd /usr/local/src/${USER}/nginx-*/
-
-# modify the existing config
-sed -i -e '/\.\/configure/ s:$: --add-module=/usr/local/src/ngx_brotli:' debian/rules
-
-#PageSpeed
-cd /usr/local/src/${USER}
-wget https://github.com/apache/incubator-pagespeed-ngx/archive/v1.13.35.2-stable.zip
-unzip v1.13.35.2-stable.zip
-sudo mv incubator-pagespeed-ngx-1.13.35.2-stable ngx_pagespeed
-cd ngx_pagespeed
-sudo wget https://dl.google.com/dl/page-speed/psol/1.13.35.2-x64.tar.gz
-sudo tar -xzvf 1.13.35.2-x64.tar.gz
-
-sudo ln -s /usr/local/src/${USER}/ngx_pagespeed /usr/local/src/ngx_pagespeed
-sudo chown ${USER}:$USER:$(id -gn $USER) /usr/local/src/ngx_pagespeed
-cd /usr/local/src/${USER}/nginx-*/
-
-# modify the existing config
-sed -i -e '/\.\/configure/ s:$: --add-module=/usr/local/src/ngx_pagespeed:' debian/rules
-
-# if gcc 8 is installed add patch to nginx 
-if [ "$(gcc -dumpversion)" == "8" ]; then
-    mkdir -p debian/patches
-    cp ${G_DIR}/gcc-8_fix.diff debian/patches/gcc-8_fix
-    echo "gcc-8_fix" > debian/patches/series
-fi
-
-# build the updated pacakge
-dpkg-buildpackage -b
-
-# optional
-# install the updated package in the current server
-cd /usr/local/src/${USER}
-
-# take a backup
-[ ! -d ~/backups/ ] && mkdir ~/backups
-cp nginx*.deb ~/backups/nginx-$(date +%F)/
-
-# sudo apt-mark unhold nginx
-sudo dpkg -i nginx_*.deb
-
-# print info about remove all the sources and apt sources file
-cd ~/
-
-printf "
-# To clean up after install You can run
- rm -rf /usr/local/src/$(echo ${USER})/nginx*
- rm -rf /usr/local/src/$(echo ${USER})/ngx_brotli
- sudo rm /etc/apt/sources.list.d/nginx.list.d/nginx.list
- sudo apt-get -qq update
-"
-# hold the package nginx from updating accidentally in the future by someone else!
-sudo apt-mark hold nginx
-sudo apt-mark hold nginx-dbg
-
-# stop the previously running instance, if any
-sudo nginx -t && sudo systemctl stop nginx
-
-# start the new Nginx instance
-sudo nginx -t && sudo systemctl start nginx
-
-echo "nginx has been installed with the following modules."
-nginx -V 2>&1 | tr -- - '\n' | grep module
-
-
+sudo apt install nginx -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 sudo ufw allow http
@@ -495,4 +372,3 @@ APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
-
